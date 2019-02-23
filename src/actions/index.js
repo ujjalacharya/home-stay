02359a041e4305da0ee725fc837b3933999f6fe9
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-23 21:24:28 +0545

    add action

---
+++ src/actions/index.js
@@ -0,0 +1,61 @@
+const rentals = [
+  {
+    id: 1,
+    title: "Central Apartment",
+    city: "New York",
+    street: "Times Sqaure",
+    category: "apartment",
+    image: "http://via.placeholder.com/350x250",
+    bedrooms: 3,
+    description: "Very nice apartment",
+    dailyRate: 34,
+    shared: false,
+    createdAt: "24/12/2017"
+  },
+  {
+    id: 2,
+    title: "Central Apartment 2",
+    city: "San Francisco",
+    street: "Main street",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    bedrooms: 2,
+    description: "Very nice apartment",
+    dailyRate: 12,
+    shared: true,
+    createdAt: "24/12/2017"
+  },
+  {
+    id: 3,
+    title: "Central Apartment 3",
+    city: "Bratislava",
+    street: "Hlavna",
+    category: "condo",
+    image: "http://via.placeholder.com/350x250",
+    bedrooms: 2,
+    description: "Very nice apartment",
+    dailyRate: 334,
+    shared: true,
+    createdAt: "24/12/2017"
+  },
+  {
+    id: 4,
+    title: "Central Apartment 4",
+    city: "Berlin",
+    street: "Haupt strasse",
+    category: "house",
+    image: "http://via.placeholder.com/350x250",
+    bedrooms: 9,
+    description: "Very nice apartment",
+    dailyRate: 33,
+    shared: true,
+    createdAt: "24/12/2017"
+  }
+];
+
+export const fetchRentals = () => {
+  return {
+    type: "FETCH_RENTALS",
+    rentals
+  };
+};
--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,32 +1,35 @@
-import React, { Component } from 'react'
-import { RentalCard } from './RentalCard';
+import React, { Component } from "react";
+import { RentalCard } from "./RentalCard";
 
-import {connect} from 'react-redux';
+import * as actions from "../../actions";
+
+import { connect } from "react-redux";
 
 class RentalList extends Component {
+  renderRental = () => {
+    return this.props.rentals.map(rental => (
+      <RentalCard rental={rental} key={rental} />
+    ));
+  };
 
- renderRental = () =>{
-  return this.props.rentals.map(rental => (
-    <RentalCard rental={rental} key={rental}/>
-  ))
- }
+  componentDidMount() {
+    this.props.dispatch(actions.fetchRentals());
+  }
 
- render() {
-  return (
-   <section id="rentalListing">
-     <h1 className="page-title">Your Home All Around the World</h1>
-     <div className="row">
-       {this.renderRental()}
-     </div>
-   </section>
-  )
- }
+  render() {
+    return (
+      <section id="rentalListing">
+        <h1 className="page-title">Your Home All Around the World</h1>
+        <div className="row">{this.renderRental()}</div>
+      </section>
+    );
+  }
 }
 
-function mapStateToProps(state){
- return{
-  rentals: state.rentals
- }
+function mapStateToProps(state) {
+  return {
+    rentals: state.rentals.data
+  };
 }
 
 export default connect(mapStateToProps)(RentalList);
