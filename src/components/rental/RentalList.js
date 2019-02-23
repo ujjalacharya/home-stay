3ca016fe09d0fe1af940cc90ef9f16df8b2b6171
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-23 11:09:19 +0545

    render rentals via state

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react'
 import { RentalCard } from './RentalCard';
 
 class RentalList extends Component {
+
+ state = {
+  rentals: [1, 2, 3]
+ }
+
+ renderRental = () =>{
+  return this.state.rentals.map(rental => (
+   <React.Fragment key={rental}>
+    <RentalCard />
+   </React.Fragment>
+  ))
+ }
+
  render() {
   return (
    <section id="rentalListing">
      <h1 className="page-title">Your Home All Around the World</h1>
      <div className="row">
-       <RentalCard />
-       <RentalCard />
-       <RentalCard />
+       {this.renderRental()}
      </div>
    </section>
   )
