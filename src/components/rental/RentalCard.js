373829a487e57bff0c66a1484e382d5f76c30838
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-23 15:23:08 +0545

    add dummy data of property

--- src/components/rental/RentalCard.js
+++ src/components/rental/RentalCard.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
-export function RentalCard() {
+export function RentalCard(props) {
+ const {rental} = props;
   return (
    <div className='col-md-3 col-xs-6'>
                 <div className="card bwm-card">
-                  <img className="card-img-top" src="http://via.placeholder.com/350x250" alt=""></img>
+                  <img className="card-img-top" src={rental.image} alt=""></img>
                   <div className="card-block">
-                    <h6 className="card-subtitle">Whole Apartment &#183; New York</h6>
-                    <h4 className="card-title">Some nice apartment</h4>
-                    <p className="card-text">$240 per Night &#183; Free Cancelation</p>
+                    <h6 className="card-subtitle">{rental.shared ? 'Shared': 'Whole'} {rental.category} &#183; {rental.city}</h6>
+                    <h4 className="card-title">{rental.description}</h4>
+                    <p className="card-text">{rental.dailyRate} per Night &#183; Free Cancelation</p>
                     <a href="" className="card-link">More Info</a>
                   </div>
                 </div>
--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -4,12 +4,63 @@ import { RentalCard } from './RentalCard';
 class RentalList extends Component {
 
  state = {
-  rentals: [1, 2, 3]
+  rentals: [{
+   id: 1,
+   title: "Central Apartment",
+   city: "New York",
+   street: "Times Sqaure",
+   category: "apartment",
+   image: "http://via.placeholder.com/350x250",
+   bedrooms: 3,
+   description: "Very nice apartment",
+   dailyRate: 34,
+   shared: false,
+   createdAt: "24/12/2017"
+ },
+ {
+   id: 2,
+   title: "Central Apartment 2",
+   city: "San Francisco",
+   street: "Main street",
+   category: "condo",
+   image: "http://via.placeholder.com/350x250",
+   bedrooms: 2,
+   description: "Very nice apartment",
+   dailyRate: 12,
+   shared: true,
+   createdAt: "24/12/2017"
+ },
+ {
+   id: 3,
+   title: "Central Apartment 3",
+   city: "Bratislava",
+   street: "Hlavna",
+   category: "condo",
+   image: "http://via.placeholder.com/350x250",
+   bedrooms: 2,
+   description: "Very nice apartment",
+   dailyRate: 334,
+   shared: true,
+   createdAt: "24/12/2017"
+ },
+ {
+   id: 4,
+   title: "Central Apartment 4",
+   city: "Berlin",
+   street: "Haupt strasse",
+   category: "house",
+   image: "http://via.placeholder.com/350x250",
+   bedrooms: 9,
+   description: "Very nice apartment",
+   dailyRate: 33,
+   shared: true,
+   createdAt: "24/12/2017"
+}]
  }
 
  renderRental = () =>{
   return this.state.rentals.map(rental => (
-    <RentalCard key={rental}/>
+    <RentalCard rental={rental} key={rental}/>
   ))
  }
 
