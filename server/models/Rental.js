48cd96035cf5b4cf2c5a08890e46848a63472038
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-26 16:26:16 +0545

    make rental model

--- server/models/Rental.js
+++ server/models/Rental.js
@@ -1,8 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RentalSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    max: [128, "Too long, max length is 128 characters"]
+  },
 
+  city: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+
+  street: {
+    type: String,
+    required: true,
+    min: [4, "Too short, min is 4 characters"]
+  },
+
+  category: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+
+  image: {
+    type: String,
+    required: true
+  },
+
+  bedrooms: Number,
+
+  shared: Boolean,
+
+  description: {
+    type: String,
+    required: true
+  },
+
+  dailyRate: Number,
+  
+  createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('RentalSchema')
+module.exports = mongoose.model("Rental", RentalSchema);
