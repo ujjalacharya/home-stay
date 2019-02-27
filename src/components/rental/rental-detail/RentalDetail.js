0dd0ce756850e3b2221ad3299d21bdb23b5a09b5
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-27 10:54:18 +0545

    add detail page layout

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -11,14 +11,61 @@ class RentalDetail extends Component {
   }
 
   render() {
-    const {rental} = this.props;
+    const { rental } = this.props;
     return rental._id ? (
-      <div>
-        <h2>{rental.title}</h2>
-        <h2>{rental.city}</h2>
-        <h2>{rental.description}</h2>
+      <section id='rentalDetails'>
+  <div className='upper-section'>
+    <div className='row'>
+      <div className='col-md-6'>
+        <img src={rental.image} alt=''></img>
       </div>
-    ) : <h2>Loading...</h2>;
+      <div className='col-md-6'>
+        <img src={rental.image} alt=''></img>
+      </div>
+    </div>
+  </div>
+
+  <div className='details-section'>
+    <div className='row'>
+      <div className='col-md-8'>
+        <div className='rental'>
+          <h2 className={`rental-type ${rental.category}`}>{rental.shared} {rental.category}</h2>
+          <h1 className='rental-title'>{rental.title}</h1>
+          <h2 className='rental-city'>{rental.city}</h2>
+          <div className='rental-room-info'>
+            <span><i className='fa fa-building'></i>{rental.bedrooms} bedrooms</span>
+            <span><i className='fa fa-user'></i> {rental.bedrooms + 4} guests</span>
+            <span><i className='fa fa-bed'></i> {rental.bedrooms + 2} beds</span>
+          </div>
+          <p className='rental-description'>
+            {rental.description}
+          </p>
+          <hr></hr>
+          <div className='rental-assets'>
+            <h3 className='title'>Assets</h3>
+            <div className='row'>
+              <div className='col-md-6'>
+                <span><i className='fa fa-asterisk'></i> Cooling</span>
+                <span><i className='fa fa-thermometer'></i> Heating</span>
+                <span><i className='fa fa-location-arrow'></i> Iron</span>
+              </div>
+              <div className='col-md-6'>
+                <span><i className='fa fa-desktop'></i> Working area</span>
+                <span><i className='fa fa-cube'></i> Washing machine</span>
+                <span><i className='fa fa-cube'></i> Dishwasher</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-4'> BOOKING</div>
+    </div>
+  </div>
+</section>
+
+    ) : (
+      <h2>Loading...</h2>
+    );
   }
 }
 
