dbe70cfbb871e5e8fccb6750bd35ab251aaaac58
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-02-28 09:53:43 +0545

    made dyanamic city locator

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -8,9 +8,15 @@ import RentalDetailInfo from "./RentalDetailInfo";
 import MapWithAMarker from "../../map";
 
 class RentalDetail extends Component {
+  state = {
+    mapcity: ""
+  }
   componentDidMount() {
     const rentalId = this.props.match.params.id;
     this.props.dispatch(actions.getRentalById(rentalId));
+
+    //Making city name locatable on map
+    setTimeout(()=>{this.setState({mapcity: this.props.rental.city.replace(" ", "%20")}) }, 1000)
   }
 
   render() {
@@ -23,13 +29,13 @@ class RentalDetail extends Component {
               <img src={rental.image} alt="" />
             </div>
             <div className="col-md-6">
-              <MapWithAMarker
+              {/* { !this.state.mapcity && <MapWithAMarker
                 googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyD3akhMLW7iOdP1oBwRCS3aJ592nSe5HYk&libraries=geometry,drawing,places"
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `350px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
-              />
-              {/* <div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="350" id="gmap_canvas" src="https://maps.google.com/maps?q=new%20baneswor&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe></div> </div>*/}
+              /> } */}
+              {this.state.mapcity && <div className="mapouter"><div className="gmap_canvas"><iframe width="600" height="350" id="gmap_canvas" src={`https://maps.google.com/maps?q=${this.state.mapcity}&t=&z=13&ie=UTF8&iwloc=&output=embed`} frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe></div> </div>}
             </div>
           </div>
         </div>
