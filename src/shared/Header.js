76b827bc405c5a0f29d24820f4187051d21391ba
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-14 12:14:04 +0545

    add conditional logout

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,28 +1,67 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 function Header(props) {
+  console.log(props);
+
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container">
-        <Link className="navbar-brand" to="/">NepStay</Link>
+        <Link className="navbar-brand" to="/">
+          NepStay
+        </Link>
         <form className="form-inline my-2 my-lg-0">
-          <input className="form-control mr-sm-2 bwm-search" type="search" placeholder="Try 'New Baneshwor'" aria-label="Search"></input>
-          <button className="btn btn-outline-success my-2 my-sm-0 btn-bwm-search" type="submit">Search</button>
+          <input
+            className="form-control mr-sm-2 bwm-search"
+            type="search"
+            placeholder="Try 'New Baneshwor'"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0 btn-bwm-search"
+            type="submit"
+          >
+            Search
+          </button>
         </form>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav ml-auto">
-            <Link className="nav-item nav-link active" to="/login">Login <span className="sr-only">(current)</span></Link>
-            <Link className="nav-item nav-link" to="/register">Register</Link>
-            <p className="nav-item nav-link" onClick={props.logout}>Logout</p>
-          </div>
+          {!props.auth.isAuth ? (
+            <div className="navbar-nav ml-auto">
+              <Link className="nav-item nav-link active" to="/login">
+                Login <span className="sr-only">(current)</span>
+              </Link>
+              <Link className="nav-item nav-link" to="/register">
+                Register
+              </Link>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <p className="nav-item nav-link" onClick={props.logout}>
+                Logout
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </nav>
-  )
+  );
 }
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
+};
 
-export default Header
+export default connect(mapStateToProps)(Header);
