028f2dca5ba0b5cbf998ae2201fed0d69537939c
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-03-14 20:35:39 +0545

    add auth nav links to separate function and fix logout style

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,62 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-function Header(props) {
-  console.log(props);
-
-  return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          NepStay
+class Header extends Component {
+  renderLinks = () => {
+    return !this.props.auth.isAuth ? (
+      <div className="navbar-nav ml-auto">
+        <Link className="nav-item nav-link active" to="/login">
+          Login <span className="sr-only">(current)</span>
         </Link>
-        <form className="form-inline my-2 my-lg-0">
-          <input
-            className="form-control mr-sm-2 bwm-search"
-            type="search"
-            placeholder="Try 'New Baneshwor'"
-            aria-label="Search"
-          />
-          <button
-            className="btn btn-outline-success my-2 my-sm-0 btn-bwm-search"
-            type="submit"
-          >
-            Search
-          </button>
-        </form>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          {!props.auth.isAuth ? (
-            <div className="navbar-nav ml-auto">
-              <Link className="nav-item nav-link active" to="/login">
-                Login <span className="sr-only">(current)</span>
-              </Link>
-              <Link className="nav-item nav-link" to="/register">
-                Register
-              </Link>
-            </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <p className="nav-item nav-link" onClick={props.logout}>
-                Logout
-              </p>
-            </div>
-          )}
-        </div>
+        <Link className="nav-item nav-link" to="/register">
+          Register
+        </Link>
       </div>
-    </nav>
-  );
+    ) : (
+      <div className="navbar-nav ml-auto">
+        <a className="nav-item nav-link clickable" onClick={this.props.logout}>
+          Logout
+        </a>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            NepStay
+          </Link>
+          <form className="form-inline my-2 my-lg-0">
+            <input
+              className="form-control mr-sm-2 bwm-search"
+              type="search"
+              placeholder="Try 'New Baneshwor'"
+              aria-label="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0 btn-bwm-search"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            {this.renderLinks()}
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
 const mapStateToProps = state => {
   return {
