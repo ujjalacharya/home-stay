4b03281fea14d7b5593057ee126dac11eba5b5ba
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-04-01 15:01:22 +0545

    add booking model

---
+++ server/models/Booking.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Booking = new Schema({
+ startAt:{
+  type: Date,
+  required: 'Starting date is required'  
+ },
+
+ endAt:{
+  type: Date,
+  required: 'Ending date is required'
+ },
+
+ totalPrice:{
+  type: String,
+ },
+
+ days:{
+  type: Number,
+ },
+
+ guests:{
+  type: Number,
+ },
+
+ createdAt:{
+  type: Date,
+  default: Date.now
+ },
+
+ user: { type: Schema.Types.ObjectId, ref: "User" },
+
+ rental: { type: Schema.Types.ObjectId, ref: "Rental" },
+ 
+});
+
+module.exports = Booking;
