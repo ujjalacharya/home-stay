3831165bd92871791dfbb65dae10280e07358028
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-04-02 18:50:07 +0545

    add booking validation with moment not working

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,6 @@
 const Booking = require("../models/Booking");
 const Rental = require("../models/Rental");
+const moment = require("moment");
 
 exports.PostBooking = async (req, res) => {
   const { startAt, endAt, days, guests, rental } = req.body;
@@ -12,22 +13,65 @@ exports.PostBooking = async (req, res) => {
     const foundrental = await Rental.findById(rental._id)
       .populate("user")
       .populate("bookings");
-    
-    if (foundrental.user._id == user.id)
-      return res
-        .status(422)
-        .json({
-          errors: [
-            {
-              title: "Invalid User!",
-              desc: "Can not create booking of your own rental"
-            }
-          ]
-        });
 
-    res.status(200).json({title: "Created booking", desc: booking, foundrental});
+    if (foundrental.user._id == user.id)
+      return res.status(422).json({
+        errors: [
+          {
+            title: "Invalid User!",
+            desc: "Can not create booking of your own rental"
+          }
+        ]
+      });
+
+    if (!validBooking(booking, foundrental)) {
+      return res.status(422).json({
+        errors: [
+          {
+            title: "Invalid Booking!",
+            desc: "Dates are already taken"
+          }
+        ]
+      });
+    }
+
+    foundrental.bookings.push(booking);
+    foundrental.save();
+    await booking.save();
+
+    res
+      .status(200)
+      .json({ title: "Created booking", desc: booking });
   } catch (err) {
-   console.log(err)
+    console.log(err);
     res.status(500).json({ title: "Error", desc: err });
   }
+};
+
+const validBooking = (proposedBooking, rental) => {
+ console.log(rental.bookings, proposedBooking)
+  if (rental.bookings && rental.bookings.length > 0) {
+    rental.bookings.every(booking => {
+      const proposedStart = moment(proposedBooking.startAt);
+      const proposedEnd = moment(proposedBooking.endAt);
+
+      const actualStart = moment(booking.startAt);
+      const actualEnd = moment(booking.endAt);
+
+      console.log(proposedStart, proposedEnd, actualStart, actualEnd)
+
+      console.log((
+       (proposedStart > actualStart && proposedStart > actualEnd) ||
+       (proposedEnd < actualStart && proposedEnd < actualEnd)
+     ))
+
+      //Check if booking is for free date available
+      return (
+        (proposedStart > actualStart && proposedStart > actualEnd) ||
+        (proposedEnd < actualStart && proposedEnd < actualEnd)
+      )
+     
+    });
+  }
+  return true;
 };
