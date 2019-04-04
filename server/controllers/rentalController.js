c7d23c7e427d4ab0c650f9bddba5441fc25dc301
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-04-04 14:44:28 +0545

    popular user and bookings in get rental by id

--- server/controllers/rentalController.js
+++ server/controllers/rentalController.js
@@ -1,9 +1,9 @@
 const Rental = require("../models/Rental");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 exports.getAllRentals = async (req, res) => {
   try {
-    const rentals = await Rental.find({});
+    const rentals = await Rental.find({}).select("-bookings");
     res.status(200).json(rentals);
   } catch (err) {
     res.status(422).json({
@@ -15,7 +15,11 @@ exports.getAllRentals = async (req, res) => {
 
 exports.getRentalById = async (req, res) => {
   try {
-    const rental = await Rental.findOne({_id: mongoose.Types.ObjectId(req.params.id)});
+    const rental = await Rental.findOne({
+      _id: mongoose.Types.ObjectId(req.params.id)
+    })
+      .populate("user", "username -_id")
+      .populate("bookings", "startAt endAt -_id");
     res.status(200).json(rental);
   } catch (err) {
     res.status(422).json({
